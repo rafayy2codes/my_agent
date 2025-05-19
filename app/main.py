d83f27c6b535e50451adce788c3355f47461b8d6
Rafay Memon <150391374+rafayy2codes@users.noreply.github.com>
2025-05-19 04:57:48 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,15 +7,6 @@ from app.api.v1 import endpoints
 app = FastAPI()
 app.include_router(endpoints.router, prefix="/v1")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Or restrict to ["http://127.0.0.1:5500"]
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
 add_exception_handlers(app)
 
 @app.get("/", tags=["Health"])
